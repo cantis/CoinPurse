de7f729137260d8d0429180f7dce477aad45547b
Evan <cantis@gmail.com>
2020-10-13 09:01:31 -0500

    Removed the old Transactions List & Object

--- app.py
+++ app.py
@@ -24,13 +24,6 @@ class Entry(db.Model):
     salary = db.Column(db.String(10), nullable=False)
 
 
-class Transaction(object):
-    def __init__(self, name, role, salary):
-        self.name = name
-        self.role = role
-        self.salary = salary
-
-
 class TransactionsTable(Table):
     classes = ['table']
     table_id = 'tranTable'
@@ -42,18 +35,6 @@ class TransactionsTable(Table):
     name = Col('Name', th_html_attrs={'class': 'table__header'}, td_html_attrs={'class': 'table__cell'})
     role = Col('Role', th_html_attrs={'class': 'table__header'}, td_html_attrs={'class': 'table_cell'})
     salary = Col('Salary', th_html_attrs={'class': 'table__header'}, td_html_attrs={'class': 'table_cell'})
-
-
-transactions = [
-    Transaction('Rolf Smith', 'Software Engineer', '$42,000.00'),
-    Transaction('Amy', 'Product Owner', '$55,000.00'),
-    Transaction('Bob', 'Tester', '$45,000.00'),
-    Transaction('Charlie', 'Developer', '$55,000.00'),
-    Transaction('Della', 'CI/CD Specialist', '$65,000.00'),
-    Transaction('Edward', 'Developer', '$75,000.00')
-    ]
-
-table = TransactionsTable(transactions)
 
 
 @app.route('/', methods=['get'])
