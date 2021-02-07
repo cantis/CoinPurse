a67ca26e5731c81cd0d03821c56320b1c7a311bb
Evan <cantis@gmail.com>
2021-02-07 00:25:04 -0600

    Added signup form

--- web/routes/auth.py
+++ web/routes/auth.py
@@ -2,8 +2,8 @@ from flask import Blueprint, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms.fields.core import BooleanField
 from wtforms.fields.html5 import EmailField
-from wtforms.fields.simple import SubmitField, PasswordField
-from wtforms.validators import InputRequired
+from wtforms.fields.simple import SubmitField, PasswordField, StringField
+from wtforms.validators import InputRequired, EqualTo
 
 from web import db
 
@@ -18,7 +18,8 @@ def login():
 
 @auth_bp.route('/signup', methods=['GET'])
 def signup():
-    return 'Signup'
+    form = SignupForm()
+    return render_template('signup.html', form=form)
 
 
 @auth_bp.route('/logout', methods=['GET'])
@@ -36,4 +37,14 @@ class LoginForm(FlaskForm):
     email = EmailField(validators=[InputRequired('Please enter your email.')])
     password = PasswordField(validators=[InputRequired('Please enter your password.')])
     remember_me = BooleanField()
-    submit = SubmitField()
+    submit = SubmitField('Login')
+
+
+class SignupForm(FlaskForm):
+    """ Signup Form """
+    first_name = StringField(validators=[InputRequired('Please enter your firstname.')])
+    last_name = StringField(validators=[InputRequired('Please enter your last name.')])
+    email = EmailField(validators=[InputRequired('Please enter your email.')])
+    password = PasswordField(validators=[InputRequired('Please enter a password.'), EqualTo('confirm', message='Passwords must match.')])
+    confirm = PasswordField()
+    submit = SubmitField('Sign Up')
