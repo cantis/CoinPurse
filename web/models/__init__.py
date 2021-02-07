c2a7bb2891e19798f0609748524f39cc952ff7ae
Evan <cantis@gmail.com>
2021-02-07 00:31:02 -0600

    Added user table

--- web/models/__init__.py
+++ web/models/__init__.py
@@ -30,5 +30,15 @@ class Setting(db.Model):
     value = db.Column(db.String(), nullable=False)
 
 
+class User(db.Model):
+    """ represents an application user """
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+
 def init_db():
     db.create_all()
